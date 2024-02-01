0bf676f1b7e246cc93097c32a30c8b91cff255c9
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2024-02-01 22:47:34 +0000

    Added common type definitions

--- include/env.hpp
+++ include/env.hpp
@@ -1,12 +1,28 @@
+#include <complex>
+#include <map>
+#include <vector>
+
 namespace qtnh {
-    struct QTNHEnv {
-        int proc_id;
-        int num_processes;
-        int num_threads;
+  typedef std::size_t tidx;
+  typedef std::vector<qtnh::tidx> tidx_tup;
+  typedef unsigned short int tidx_tup_st;
 
-        QTNHEnv();
-        ~QTNHEnv();
+  enum class TIdxFlag { open, closed, oob };
 
-        void print() const;
-    };
+  typedef std::vector<TIdxFlag> tidx_flags;
+  typedef std::complex<double> tel;
+  typedef std::map<qtnh::tidx_tup, int> dmap;
+
+  typedef std::pair<tidx, tidx> wire;
+
+  struct QTNHEnv {
+    int proc_id;
+    int num_processes;
+    int num_threads;
+
+    QTNHEnv();
+    ~QTNHEnv();
+
+    void print() const;
+  };
 }
