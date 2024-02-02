069ac1593a4cd0c33ba78306917c2e2be750edd0
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2024-02-02 11:14:47 +0000

    Moved typedefs to separate file

--- include/env.hpp
+++ include/env.hpp
@@ -1,20 +1,4 @@
-#include <complex>
-#include <map>
-#include <vector>
-
 namespace qtnh {
-  typedef std::size_t tidx;
-  typedef std::vector<qtnh::tidx> tidx_tup;
-  typedef unsigned short int tidx_tup_st;
-
-  enum class TIdxFlag { open, closed, oob };
-
-  typedef std::vector<TIdxFlag> tidx_flags;
-  typedef std::complex<double> tel;
-  typedef std::map<qtnh::tidx_tup, int> dmap;
-
-  typedef std::pair<tidx, tidx> wire;
-
   struct QTNHEnv {
     int proc_id;
     int num_processes;
