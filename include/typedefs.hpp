4760d8ffe815c6d38fec026b7dd2664e7c3c058d
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2024-02-05 18:06:44 +0000

    Added flag for self-contraction

--- include/typedefs.hpp
+++ include/typedefs.hpp
@@ -9,7 +9,7 @@ namespace qtnh {
   typedef std::vector<qtnh::tidx> tidx_tup;
   typedef unsigned short int tidx_tup_st;
 
-  enum class TIdxFlag { open, closed, oob };
+  enum class TIdxFlag { open, closed, self, oob = 99 };
 
   typedef std::vector<TIdxFlag> tidx_flags;
   typedef std::complex<double> tel;
