9581015d24365c5508e21c25f3b81521518d6ac1
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2024-02-08 12:22:51 +0000

    Updated indexing data types

--- tests/c2-indexing-tests.cpp
+++ tests/c2-indexing-tests.cpp
@@ -1,48 +1,48 @@
 #include <catch2/catch_test_macros.hpp>
-#include "coords.hpp"
+#include "indexing.hpp"
 
 TEST_CASE("create-indexing-test") {
-    tidx_tuple dims = { 2, 3, 4, 5 };
+    qtnh::tidx_tup dims = { 2, 3, 4, 5 };
     std::size_t closed_idx = 1;
-    tidx_flags flags(4, TIFlag::open);
-    flags.at(1) = TIFlag::closed;
+    qtnh::tidx_flags flags(4, qtnh::TIdxFlag::open);
+    flags.at(1) = qtnh::TIdxFlag::closed;
     
-    TIndexing* ti;
+    qtnh::TIndexing* ti;
 
-    REQUIRE_NOTHROW(ti = new TIndexing(dims));
-    REQUIRE_NOTHROW(ti = new TIndexing(dims, closed_idx));
-    REQUIRE_NOTHROW(ti = new TIndexing(dims, flags));
+    REQUIRE_NOTHROW(ti = new qtnh::TIndexing(dims));
+    REQUIRE_NOTHROW(ti = new qtnh::TIndexing(dims, closed_idx));
+    REQUIRE_NOTHROW(ti = new qtnh::TIndexing(dims, flags));
 
-    tidx_flags invalid_flags(5, TIFlag::open);
+    qtnh::tidx_flags invalid_flags(5, qtnh::TIdxFlag::open);
 
-    REQUIRE_THROWS(ti = new TIndexing(dims, invalid_flags));
+    REQUIRE_THROWS(ti = new qtnh::TIndexing(dims, invalid_flags));
 }
 
 TEST_CASE("increase-index-test") {
-    tidx_tuple dims = { 2, 3, 4, 5 };
-    TIndexing ti(dims);
+    qtnh::tidx_tup dims = { 2, 3, 4, 5 };
+    qtnh::TIndexing ti(dims);
 
-    tidx_tuple start_idx = { 0, 0, 0, 0 };
+    qtnh::tidx_tup start_idx = { 0, 0, 0, 0 };
     REQUIRE_NOTHROW(start_idx = ti.next(start_idx));
 
-    tidx_tuple end_idx = { 1, 2, 3, 4 };
+    qtnh::tidx_tup end_idx = { 1, 2, 3, 4 };
     REQUIRE_THROWS(end_idx = ti.next(end_idx));
 }
 
 TEST_CASE("decrease-index-test") {
-    tidx_tuple dims = { 2, 3, 4, 5 };
-    TIndexing ti(dims);
+    qtnh::tidx_tup dims = { 2, 3, 4, 5 };
+    qtnh::TIndexing ti(dims);
 
-    tidx_tuple end_idx = { 1, 2, 3, 4 };
+    qtnh::tidx_tup end_idx = { 1, 2, 3, 4 };
     REQUIRE_NOTHROW(end_idx = ti.prev(end_idx));
 
-    tidx_tuple start_idx = { 0, 0, 0, 0 };
+    qtnh::tidx_tup start_idx = { 0, 0, 0, 0 };
     REQUIRE_THROWS(start_idx = ti.prev(start_idx));
 }
 
 TEST_CASE("iterate-indexing-test") {
-    tidx_tuple dims = { 2, 3, 4, 5 };
-    TIndexing ti(dims);
+    qtnh::tidx_tup dims = { 2, 3, 4, 5 };
+    qtnh::TIndexing ti(dims);
 
     REQUIRE_NOTHROW([&](){
         for (auto i : ti);
@@ -50,22 +50,22 @@ TEST_CASE("iterate-indexing-test") {
 }
 
 TEST_CASE("cut-indexing-test") {
-    tidx_tuple dims1 = { 2, 3, 4, 5 };
-    tidx_flags flags1(4, TIFlag::open);
-    flags1.at(1) = TIFlag::closed;
+    qtnh::tidx_tup dims1 = { 2, 3, 4, 5 };
+    qtnh::tidx_flags flags1(4, qtnh::TIdxFlag::open);
+    flags1.at(1) = qtnh::TIdxFlag::closed;
 
-    TIndexing ti1(dims1, flags1);
-    TIndexing ti2;
+    qtnh::TIndexing ti1(dims1, flags1);
+    qtnh::TIndexing ti2;
     
-    REQUIRE_NOTHROW(ti2 = ti1.cut(TIFlag::closed));
+    REQUIRE_NOTHROW(ti2 = ti1.cut(qtnh::TIdxFlag::closed));
 }
 
 TEST_CASE("append-indexings-test") {
-    tidx_tuple dims1 = { 2, 3, 4, 5 };
-    tidx_tuple dims2 = { 5, 4, 3, 2 };
-    TIndexing ti1(dims1);
-    TIndexing ti2(dims2);
+    qtnh::tidx_tup dims1 = { 2, 3, 4, 5 };
+    qtnh::tidx_tup dims2 = { 5, 4, 3, 2 };
+    qtnh::TIndexing ti1(dims1);
+    qtnh::TIndexing ti2(dims2);
     
-    TIndexing ti12;
-    REQUIRE_NOTHROW(ti12 = TIndexing::app(ti1, ti2));
+    qtnh::TIndexing ti12;
+    REQUIRE_NOTHROW(ti12 = qtnh::TIndexing::app(ti1, ti2));
 }
--- tests/c2-indexing-validation.cpp
+++ tests/c2-indexing-validation.cpp
@@ -1,17 +1,17 @@
 #include <catch2/catch_test_macros.hpp>
-#include "coords.hpp"
+#include "indexing.hpp"
 
 TEST_CASE("create-indexing-validation") {
-    tidx_tuple dims = { 2, 3, 4, 5 };
+    qtnh::tidx_tup dims = { 2, 3, 4, 5 };
     std::size_t closed_idx = 1;
-    tidx_flags flags1(4, TIFlag::open);
-    tidx_flags flags2 = flags1;
-    flags2.at(1) = TIFlag::closed;
+    qtnh::tidx_flags flags1(4, qtnh::TIdxFlag::open);
+    qtnh::tidx_flags flags2 = flags1;
+    flags2.at(1) = qtnh::TIdxFlag::closed;
 
-    TIndexing ti1(dims);
-    TIndexing ti2(dims, flags1);
-    TIndexing ti3(dims, closed_idx);
-    TIndexing ti4(dims, flags2);
+    qtnh::TIndexing ti1(dims);
+    qtnh::TIndexing ti2(dims, flags1);
+    qtnh::TIndexing ti3(dims, closed_idx);
+    qtnh::TIndexing ti4(dims, flags2);
 
     REQUIRE(ti1 == ti2);
     REQUIRE(ti3 == ti4);
@@ -20,15 +20,15 @@ TEST_CASE("create-indexing-validation") {
 }
 
 TEST_CASE("increase-index-validation") {
-    tidx_tuple dims = { 5, 4, 3, 2 };
-    tidx_flags flags(4, TIFlag::open);
-    flags.at(2) = TIFlag::closed;
+    qtnh::tidx_tup dims = { 5, 4, 3, 2 };
+    qtnh::tidx_flags flags(4, qtnh::TIdxFlag::open);
+    flags.at(2) = qtnh::TIdxFlag::closed;
     
-    TIndexing ti(dims, flags);
+    qtnh::TIndexing ti(dims, flags);
 
-    tidx_tuple idx = { 0, 0, 0, 0 };
-    tidx_tuple target_idx1 = { 0, 0, 0, 1 };
-    tidx_tuple target_idx2 = { 0, 1, 0, 0 };
+    qtnh::tidx_tup idx = { 0, 0, 0, 0 };
+    qtnh::tidx_tup target_idx1 = { 0, 0, 0, 1 };
+    qtnh::tidx_tup target_idx2 = { 0, 1, 0, 0 };
 
     idx = ti.next(idx);
     REQUIRE(idx == target_idx1);
@@ -38,15 +38,15 @@ TEST_CASE("increase-index-validation") {
 }
 
 TEST_CASE("decrease-index-validation") {
-    tidx_tuple dims = { 5, 4, 3, 2 };
-    tidx_flags flags(4, TIFlag::open);
-    flags.at(2) = TIFlag::closed;
+    qtnh::tidx_tup dims = { 5, 4, 3, 2 };
+    qtnh::tidx_flags flags(4, qtnh::TIdxFlag::open);
+    flags.at(2) = qtnh::TIdxFlag::closed;
     
-    TIndexing ti(dims, flags);
+    qtnh::TIndexing ti(dims, flags);
 
-    tidx_tuple idx = { 4, 3, 2, 1 };
-    tidx_tuple target_idx1 = { 4, 3, 2, 0 };
-    tidx_tuple target_idx2 = { 4, 2, 2, 1 };
+    qtnh::tidx_tup idx = { 4, 3, 2, 1 };
+    qtnh::tidx_tup target_idx1 = { 4, 3, 2, 0 };
+    qtnh::tidx_tup target_idx2 = { 4, 2, 2, 1 };
 
     idx = ti.prev(idx);
     REQUIRE(idx == target_idx1);
@@ -56,12 +56,12 @@ TEST_CASE("decrease-index-validation") {
 }
 
 TEST_CASE("iterate-indexing-validation") {
-    tidx_tuple dims = { 2, 3, 4, 5 };
-    tidx_flags flags(4, TIFlag::open);
-    flags.at(1) = TIFlag::closed;
+    qtnh::tidx_tup dims = { 2, 3, 4, 5 };
+    qtnh::tidx_flags flags(4, qtnh::TIdxFlag::open);
+    flags.at(1) = qtnh::TIdxFlag::closed;
 
-    TIndexing ti(dims, flags);
-    tidx_tuple j = { 0, 0, 0, 0 };
+    qtnh::TIndexing ti(dims, flags);
+    qtnh::tidx_tup j = { 0, 0, 0, 0 };
 
     for (auto i : ti) {
         REQUIRE(i == j);
@@ -73,30 +73,30 @@ TEST_CASE("iterate-indexing-validation") {
 }
 
 TEST_CASE("cut-indexing-validation") {
-    tidx_tuple dims1 = { 2, 3, 4, 5 };
-    tidx_flags flags1(4, TIFlag::open);
-    flags1.at(1) = TIFlag::closed;
-    TIndexing ti1(dims1, flags1);
+    qtnh::tidx_tup dims1 = { 2, 3, 4, 5 };
+    qtnh::tidx_flags flags1(4, qtnh::TIdxFlag::open);
+    flags1.at(1) = qtnh::TIdxFlag::closed;
+    qtnh::TIndexing ti1(dims1, flags1);
 
-    tidx_tuple dims2 = { 2, 4, 5 };
-    tidx_flags flags2(3, TIFlag::open);
-    TIndexing ti2(dims2, flags2);
+    qtnh::tidx_tup dims2 = { 2, 4, 5 };
+    qtnh::tidx_flags flags2(3, qtnh::TIdxFlag::open);
+    qtnh::TIndexing ti2(dims2, flags2);
     
-    REQUIRE(ti2 == ti1.cut(TIFlag::closed));
+    REQUIRE(ti2 == ti1.cut(qtnh::TIdxFlag::closed));
 }
 
 TEST_CASE("append-indexings-validation") {
-    tidx_tuple dims1 = { 2, 3, 4 };
-    tidx_flags flags1 = { TIFlag::open, TIFlag::closed, TIFlag::open };
-    tidx_tuple dims2 = { 3, 2 };
-    tidx_flags flags2 = { TIFlag::closed, TIFlag::open };
+    qtnh::tidx_tup dims1 = { 2, 3, 4 };
+    qtnh::tidx_flags flags1 = { qtnh::TIdxFlag::open, qtnh::TIdxFlag::closed, qtnh::TIdxFlag::open };
+    qtnh::tidx_tup dims2 = { 3, 2 };
+    qtnh::tidx_flags flags2 = { qtnh::TIdxFlag::closed, qtnh::TIdxFlag::open };
 
-    tidx_tuple dims12 = { 2, 3, 4, 3, 2 };
-    tidx_flags flags12 = { TIFlag::open, TIFlag::closed, TIFlag::open, TIFlag::closed, TIFlag::open };
+    qtnh::tidx_tup dims12 = { 2, 3, 4, 3, 2 };
+    qtnh::tidx_flags flags12 = { qtnh::TIdxFlag::open, qtnh::TIdxFlag::closed, qtnh::TIdxFlag::open, qtnh::TIdxFlag::closed, qtnh::TIdxFlag::open };
 
-    TIndexing ti1(dims1, flags1);
-    TIndexing ti2(dims2, flags2);
-    TIndexing ti12(dims12, flags12);
+    qtnh::TIndexing ti1(dims1, flags1);
+    qtnh::TIndexing ti2(dims2, flags2);
+    qtnh::TIndexing ti12(dims12, flags12);
 
-    REQUIRE(ti12 == TIndexing::app(ti1, ti2));
+    REQUIRE(ti12 == qtnh::TIndexing::app(ti1, ti2));
 }
