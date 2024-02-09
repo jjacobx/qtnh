3e9e14aea4f6cefb60ccf0112c5b83d28fa2eb02
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2024-02-09 16:04:59 +0000

    Fixed indentation

--- src/env.cpp
+++ src/env.cpp
@@ -5,21 +5,21 @@
 #include "env.hpp"
 
 namespace qtnh {
-    QTNHEnv::QTNHEnv() {
-        this->num_threads = omp_get_max_threads();
+  QTNHEnv::QTNHEnv() {
+    this->num_threads = omp_get_max_threads();
 
-        MPI_Init(NULL, NULL);
-        MPI_Comm_rank(MPI_COMM_WORLD, &this->proc_id);
-        MPI_Comm_size(MPI_COMM_WORLD, &this->num_processes);
-    }
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &this->proc_id);
+    MPI_Comm_size(MPI_COMM_WORLD, &this->num_processes);
+  }
 
-    QTNHEnv::~QTNHEnv() {
-        MPI_Finalize();
-    }
+  QTNHEnv::~QTNHEnv() {
+    MPI_Finalize();
+  }
 
-    void QTNHEnv::print() const {
-        std::cout << "Process ID: " << this->proc_id << std::endl;
-        std::cout << "Process count: " << this->num_processes << std::endl;
-        std::cout << "Thread count: " << this-> num_threads << std::endl;
-    }
+  void QTNHEnv::print() const {
+    std::cout << "Process ID: " << this->proc_id << std::endl;
+    std::cout << "Process count: " << this->num_processes << std::endl;
+    std::cout << "Thread count: " << this-> num_threads << std::endl;
+  }
 }
