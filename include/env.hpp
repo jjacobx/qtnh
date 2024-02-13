d06822cff05ece5f95c3db22d5d2919258fccada
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2024-02-13 21:37:34 +0000

    Added array to store MPI datatypes for swaps

--- include/env.hpp
+++ include/env.hpp
@@ -1,16 +1,26 @@
 #ifndef ENV_HPP
 #define ENV_HPP
 
+#include <mpi.h>
+
 namespace qtnh {
   struct QTNHEnv {
-    unsigned int proc_id;
-    unsigned int num_processes;
-    unsigned int num_threads;
+    public:
+      unsigned int proc_id;
+      unsigned int num_processes;
+      unsigned int num_threads;
 
-    QTNHEnv();
-    ~QTNHEnv();
+      MPI_Datatype swap_types[4][32];
+      bool swap_types_committed[4][32];
 
-    void print() const;
+      QTNHEnv();
+      ~QTNHEnv();
+
+      void print() const;
+
+    private:
+      void init_swap_types();
+      void free_swap_types();
   };
 }
 
--- src/env.cpp
+++ src/env.cpp
@@ -18,12 +18,15 @@ namespace qtnh {
     proc_id = _proc_id;
     num_processes = _num_processes;
 
+    init_swap_types();
+
     #ifdef DEBUG
       ROOT_COUT << "DEF_STENSOR_BCAST is " << (DEF_STENSOR_BCAST ? "on" : "off") << std::endl;
     #endif
   }
 
   QTNHEnv::~QTNHEnv() {
+    free_swap_types();
     MPI_Finalize();
   }
 
@@ -31,5 +34,30 @@ namespace qtnh {
     std::cout << "Process ID: " << proc_id << std::endl;
     std::cout << "Process count: " << num_processes << std::endl;
     std::cout << "Thread count: " << num_threads << std::endl;
+  }
+
+  void QTNHEnv::init_swap_types() {
+    for (std::size_t i = 0; i < 4; ++i) {
+      for (std::size_t j = 0; j < 32; ++j) {
+        auto b = i + 2;
+        auto block_length = (long long)pow(b, j);
+        auto stride = b * block_length;
+        if (stride <= std::numeric_limits<int>::max()) {
+          MPI_Type_vector(1, block_length, stride, MPI_C_DOUBLE_COMPLEX, &swap_types[i][j]);
+        }
+        swap_types_committed[i][j] = false;
+      }
+    }
+  }
+
+  void QTNHEnv::free_swap_types() {
+    for (std::size_t i = 0; i < 4; ++i) {
+      for (std::size_t j = 0; j < 32; ++j) {
+        if (swap_types_committed[i][j]) {
+          MPI_Type_free(&swap_types[i][j]);
+          swap_types_committed[i][j] = false;
+        }
+      }
+    }
   }
 }
