8bd829ffe72d0fdeb1551eb2ef876ebab345c60c
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2024-03-22 19:16:51 +0000

    Made file access safer

--- tests/gen/random-tensors-gen.py
+++ tests/gen/random-tensors-gen.py
@@ -16,8 +16,6 @@ def random_dims_and_wires(nwires, max_idxs, allowed_dims):
   dims1 = random.choices(allowed_dims, k = n1)
   dims2 = random.choices(allowed_dims, k = n2)
 
-  # nw = np.random.randint(0, min(n1, n2))
-
   w1 = random.sample(list(np.arange(0, n1)), nwires)
   w2 = random.sample(list(np.arange(0, n2)), nwires)
 
@@ -52,52 +50,51 @@ def make_contraction(t1, t2, ws):
 
 def gen_random_tensors_header(contractions : list[Contraction]):
   this_dir = os.path.dirname(os.path.realpath(__file__))
-  f = open(this_dir + '/random-tensors.hpp', 'w', encoding="utf-8")
+  with open(this_dir + '/random-tensors.hpp', 'w', encoding = "utf-8") as f:
+    f.write("#ifndef RANDOM_TENSORS_HPP\n#define RANDOM_TENSORS_HPP\n\n")
+    f.write("#include \"contraction-validation.hpp\"\n\n")
+    f.write("using namespace std::complex_literals;\n\n")
 
-  f.write("#ifndef RANDOM_TENSORS_HPP\n#define RANDOM_TENSORS_HPP\n\n")
-  f.write("#include \"contraction-validation.hpp\"\n\n")
-  f.write("using namespace std::complex_literals;\n\n")
+    f.write("namespace gen {\n")
 
-  f.write("namespace gen {\n")
+    for i, con in enumerate(contractions) :
+      f.write(f"  const contraction_validation v{i+1} {{\n")
+      for t in (con.t1, con.t2, con.t3):
+        f.write("    tensor_info {{ ")
+        for j, dim in enumerate(t.shape):
+          f.write(f"{dim}")
+          if (j < len(t.shape) - 1):
+            f.write(", ")
 
-  for i, con in enumerate(contractions) :
-    f.write(f"  const contraction_validation v{i+1} {{\n")
-    for t in (con.t1, con.t2, con.t3):
-      f.write("    tensor_info {{ ")
-      for j, dim in enumerate(t.shape):
-        f.write(f"{dim}")
-        if (j < len(t.shape) - 1):
+        f.write(" }, { ")
+        for j, el in enumerate(t.flatten()):
+          plus = "+" if el.imag >= 0 else ""
+          f.write(f"{el.real: .2f}{plus}{el.imag:.2f}i")
+          if (j < t.size - 1):
+            f.write(", ")
+
+        f.write(" }}, \n")
+
+      f.write("\n")
+
+      f.write("    std::vector<qtnh::wire>{")
+      for j, w in enumerate(con.ws):
+        f.write(f"{{ {w[0]}, {w[1]} }}")
+        if (j < len(con.ws) - 1):
           f.write(", ")
+      f.write("}\n")
+      f.write("  };\n\n")
 
-      f.write(" }, { ")
-      for j, el in enumerate(t.flatten()):
-        plus = "+" if el.imag >= 0 else ""
-        f.write(f"{el.real: .2f}{plus}{el.imag:.2f}i")
-        if (j < t.size - 1):
-          f.write(", ")
-
-      f.write(" }}, \n")
-
-    f.write("\n")
-
-    f.write("    std::vector<qtnh::wire>{")
-    for j, w in enumerate(con.ws):
-      f.write(f"{{ {w[0]}, {w[1]} }}")
-      if (j < len(con.ws) - 1):
+    f.write("  const std::vector<contraction_validation> cvs { ")
+    for i in range(len(contractions)):
+      f.write(f"v{i+1}")
+      if i < len(contractions) - 1:
         f.write(", ")
-    f.write("}\n")
-    f.write("  };\n\n")
+    
+    f.write(" };\n")
 
-  f.write("  const std::vector<contraction_validation> cvs { ")
-  for i in range(len(contractions)):
-    f.write(f"v{i+1}")
-    if i < len(contractions) - 1:
-      f.write(", ")
-  
-  f.write(" };\n")
-
-  f.write("}\n\n")
-  f.write("#endif")
+    f.write("}\n\n")
+    f.write("#endif")
 
 
 
@@ -107,24 +104,28 @@ def main():
 
   cons = []
 
+  # 2-dimensional indices
   for i in range(5):
     dims1, dims2, ws = random_dims_and_wires(i % 4, 5, [2])
     con = make_contraction(random_tensor(dims1), random_tensor(dims2), ws)
 
     cons.append(con)
 
+  # 2 and 3-dimensional indices
   for i in range(5):
     dims1, dims2, ws = random_dims_and_wires(i % 3, 4, [2, 3])
     con = make_contraction(random_tensor(dims1), random_tensor(dims2), ws)
 
     cons.append(con)
 
+  # 2, 3 and 4-dimensional indices
   for i in range(5):
     dims1, dims2, ws = random_dims_and_wires(i % 2, 3, [2, 3, 4])
     con = make_contraction(random_tensor(dims1), random_tensor(dims2), ws)
 
     cons.append(con)
 
+  # 2, 3, 4 and 5-dimensional indices
   for i in range(5):
     dims1, dims2, ws = random_dims_and_wires(i % 2, 3, [2, 3, 4, 5])
     con = make_contraction(random_tensor(dims1), random_tensor(dims2), ws)
