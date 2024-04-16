fb20f5fe66ff0abd06267adb65f49a508811dab0
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2024-04-16 21:08:00 +0100

    Starting working on more distributed tensor tests

--- tests/c2-mpi.cpp
+++ tests/c2-mpi.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include "qtnh.hpp"
 
+#include "gen/random-tensors.hpp"
+
 qtnh::QTNHEnv ENV;
 
 class RootReporter : public Catch::StreamingReporterBase {
@@ -42,7 +44,11 @@ unsigned int RootReporter::counter = 0;
 
 CATCH_REGISTER_REPORTER("root", RootReporter)
 
-TEST_CASE("test-mpi", "[mpi][2rank]") {
+bool eq(qtnh::tel a, qtnh::tel b) {
+  return (std::abs(a.real() - b.real()) < 1E-5) && (std::abs(a.imag() - b.imag()) < 1E-5);
+}
+
+TEST_CASE("distribute-tensor", "[mpi][2rank]") {
   using namespace qtnh;
   using namespace std::complex_literals;
 
@@ -75,6 +81,32 @@ TEST_CASE("test-mpi", "[mpi][2rank]") {
   }
 }
 
+TEST_CASE("contract-tensor", "[mpi][2rank]") {
+  using namespace qtnh;
+
+  for (auto& cv : gen::mpi2r_vals) {
+    auto t_sden1_u = std::make_unique<SDenseTensor>(ENV, cv.t1_info.dims, cv.t1_info.els);
+    auto t_sden2_u = std::make_unique<SDenseTensor>(ENV, cv.t2_info.dims, cv.t2_info.els);
+    auto t_dden2_u = std::unique_ptr<DDenseTensor>(t_sden2_u->distribute(1));
+
+    auto t_r1_u = Tensor::contract(std::move(t_sden1_u), std::move(t_dden2_u), cv.wires);
+
+    qtnh::tidx_tup t_r1_dims = cv.t3_info.dims;
+    std::vector<qtnh::tel> t_r1_els = cv.t3_info.els;
+
+    REQUIRE(t_r1_u->getDims() == t_r1_dims);
+    TIndexing ti_r1(t_r1_dims, 0);
+
+    for (auto idxs : ti_r1) {
+      idxs.at(0) = ENV.proc_id;
+      auto el = t_r1_els.at(utils::idxs_to_i(idxs, t_r1_dims));
+
+      idxs.erase(idxs.begin());
+      REQUIRE(eq(t_r1_u->getLocEl(idxs).value(), el));
+    }
+  }
+}
+
 TEST_CASE("new-tensor", "[mpi][4rank]") {
   using namespace qtnh;
   using namespace std::complex_literals;
@@ -98,3 +130,8 @@ TEST_CASE("new-tensor", "[mpi][4rank]") {
     }
   }
 }
+
+// Create DDenseTensor
+// Contraction/tensor product of DDenseTensors
+// Swap local/distributed and distributed/distributed
+// Distributed tensor network tests
