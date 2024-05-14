01af6b978fe0718815afd93fd0370bd53df8cdec
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2024-05-14 11:58:36 +0100

    Tested tensor networks with 4 random tensors

--- tests/c2-network.cpp
+++ tests/c2-network.cpp
@@ -149,4 +149,35 @@ TEST_CASE("tn-contraction") {
       }
     }
   }
+
+  SECTION("4-tensors") {
+    for (auto& tnv : gen::tn4_vals) {
+      TensorNetwork tn;
+
+      // maps between tensor ids in Python and in TensorNetwork
+      std::vector<qtnh::uint> maps(tnv.t_infos.size());
+      std::size_t i = 0;
+
+      for (auto& t_info : tnv.t_infos) {
+        maps.at(i++) = tn.createTensor<SDenseTensor>(ENV, t_info.dims, t_info.els);
+      }
+
+      for (auto b_info : tnv.b_infos) {
+        tn.createBond(maps.at(b_info.t1_idx), maps.at(b_info.t2_idx), b_info.wires);
+      }
+
+      auto res_id = tn.contractAll();
+      auto t_res_u = tn.extractTensor(res_id);
+
+      qtnh::tidx_tup t_res_dims = tnv.result_info.dims;
+      std::vector<qtnh::tel> t_res_els = tnv.result_info.els;
+
+      REQUIRE(t_res_u->getDims() == t_res_dims);
+      TIndexing ti_res(t_res_dims);
+      for (auto idxs : ti_res) {
+        auto el = t_res_els.at(utils::idxs_to_i(idxs, t_res_dims));
+        REQUIRE(eq(t_res_u->getLocEl(idxs).value(), el, 1E-2));
+      }
+    }
+  }
 }
