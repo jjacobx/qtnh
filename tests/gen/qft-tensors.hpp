0158e7eab6d11dff2359e1b995fd3f9327a85617
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2024-05-14 16:43:48 +0100

    Created header with spcification of QFT tensors

---
+++ tests/gen/qft-tensors.hpp
@@ -0,0 +1,17 @@
+#ifndef QFT_TENSORS_HPP 
+#define QFT_TENSORS_HPP
+
+#include "contraction-validation.hpp"
+
+using namespace std::complex_literals;
+
+namespace gen {
+  tensor_info plus_state {{ 2 }, { std::pow(2, -.5), std::pow(2, -.5) }};
+  tensor_info hadamard {{ 2, 2 }, { std::pow(2, -.5),  std::pow(2, -.5), std::pow(2, -.5), -std::pow(2, -.5) }};
+
+  tensor_info cphase(double p) {
+    return {{ 2, 2, 2, 2 }, { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, std::exp(1i * p)}};
+  }
+}
+
+#endif
