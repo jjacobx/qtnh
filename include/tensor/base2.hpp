59d2721f816dfcf1db443fa0cca8b8ed78f1a0c3
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2024-05-21 23:26:02 +0100

    Tidied up and documented new base tensor code

--- include/tensor/base2.hpp
+++ include/tensor/base2.hpp
@@ -6,42 +6,35 @@
 
 #include "../core/env.hpp"
 #include "../core/typedefs.hpp"
+#include "../core/utils.hpp"
 
 namespace qtnh {
   class Tensor {
+    protected:
+      struct Distributor;
+
     public:
-      /// Empty constructor is invalid due to undefined environment. 
       Tensor() = delete;
-      /// Copy constructor is invalid due to potential large tensor size. 
       Tensor(const Tensor&) = delete;
-      /// Default destructor. 
       ~Tensor() = default;
-
-      struct Distributor {
-        qtnh::uint stretch;
-        qtnh::uint cycles;
-        qtnh::uint offset;
-        qtnh::uint base;
-
-        const QTNHEnv& env;
-
-        MPI_Comm group_comm;
-        bool active;
-
-        qtnh::uint getSpan() { return stretch * base * cycles; }
-        qtnh::uint getLowerRange() { return offset; }
-        qtnh::uint getUpperRange() { return offset + getSpan(); }
-      };
-
-      const qtnh::tidx_tup& getDims() const { return dims; }           ///< Index dimensions getter. 
-      const qtnh::tidx_tup& getLocDims() const { return loc_dims; }    ///< Local index dimensions getter. 
-      const qtnh::tidx_tup& getDistDims() const { return dist_dims; }  ///< Distributed index dimensions getter. 
-
-      const Distributor& getDistributor() const { return distributor; }  ///< Tensor distributor getter. 
-
-      std::size_t getSize() const;      ///< Get combined size of all index dimensions. 
-      std::size_t getLocSize() const;   ///< Get combined size of local index dimensions. 
-      std::size_t getDistSize() const;  ///< Get combined size of distributed index dimensions. 
+      
+      const qtnh::tidx_tup& getLocDims() const noexcept { return loc_dims; }
+      const qtnh::tidx_tup& getDisDims() const noexcept { return dis_dims; }
+      const Distributor& getDistributor() const noexcept { return distributor; }
+      
+      /// @brief Helper getter to access complete tensor dimensions. 
+      /// @return Concatenated distributed and local dimensions. 
+      const qtnh::tidx_tup& getTotDims() const noexcept { return utils::concat_dims(dis_dims, loc_dims); }
+      
+      /// @brief Helper to calculate size of the local part of the tensor. 
+      /// @return Number of local eements in the tensor. 
+      std::size_t locSize() const noexcept { return utils::dims_to_size(loc_dims); }
+      /// @brief Helper to calculate size of the distributed part of the tensor. 
+      /// @return Number of ranks to which one instance of the tensor is distributed. 
+      std::size_t disSize() const noexcept { return utils::dims_to_size(dis_dims); }
+      /// @brief Helper to calculate size of the entire tensor. 
+      /// @return Number of elements in the entire tensor. 
+      std::size_t totSize() const noexcept { return utils::dims_to_size(getTotDims()); }
 
       /// @brief Rank-safe method to get element and given global indices. 
       /// @param idxs Tensor index tuple indicating global position of the element. 
@@ -77,9 +70,9 @@ namespace qtnh {
       /// For some classes it might not be a valid method. 
       virtual void swap(qtnh::tidx_tup_st idx1, qtnh::tidx_tup_st idx2) = 0;
       /// @brief Redistribute current tensor. 
-      /// @param stretch How many times each local element is repeated. 
-      /// @param cycles How many times the entire tensor structure is repeated. 
-      /// @param offset Number of emoty processes before the tensor begins. 
+      /// @param stretch Number of times each local tensor chunk is repeated across contiguous processes. 
+      /// @param cycles Number of times the entire tensor structure is repeated. 
+      /// @param offset Number of empty processes before the tensor begins. 
       /// 
       /// Must be overriden for every derived tensor class, as distribution depends on implementation. 
       virtual void redistribute(qtnh::uint stretch, qtnh::uint cycles, qtnh::uint offset) = 0;
@@ -92,20 +85,46 @@ namespace qtnh {
       static std::unique_ptr<Tensor> contract(std::unique_ptr<Tensor> t1u, std::unique_ptr<Tensor> t2u, const std::vector<qtnh::wire>& ws);
 
     protected:
-      /// @brief Construct empty tensor of zero size within environment. 
+      /// @brief Construct empty tensor of zero size within environment and with default distribution paremeters. 
       /// @param env Environment to use for construction. 
       Tensor(const QTNHEnv& env);
-      /// @brief Construct empty tensor with given local and distributed dimensions within environment. 
+      /// @brief Construct empty tensor with given local and distributed dimensions within environment with default distribution paremeters. 
       /// @param env Environment to use for construction. 
       /// @param loc_dims Local index dimensions. 
       /// @param dist_dims Distributed index dimensions. 
       Tensor(const QTNHEnv& env, qtnh::tidx_tup loc_dims, qtnh::tidx_tup dist_dims);
+      /// @brief Construct empty tensor with given local and distributed dimensions within environment with given distribution paremeters. 
+      /// @param env Environment to use for construction. 
+      /// @param loc_dims Local index dimensions. 
+      /// @param dist_dims Distributed index dimensions. 
+      /// @param stretch Number of times each local tensor chunk is repeated across contiguous processes. 
+      /// @param cycles Number of times the entire tensor structure is repeated. 
+      /// @param offset Number of empty processes before the tensor begins. 
+      Tensor(const QTNHEnv& env, qtnh::tidx_tup loc_dims, qtnh::tidx_tup dist_dims, qtnh::uint stretch, qtnh::uint cycles, qtnh::uint offset);
+
+      qtnh::tidx_tup loc_dims;  ///< Local index dimensions. 
+      qtnh::tidx_tup dis_dims;  ///< Distributed index dimensions. 
+
+      /// @brief Tensor distributor class responsible for handling how tensor is stored in distributed memory. 
+      struct Distributor {
+        qtnh::uint stretch;  ///< Number of times each local tensor chunk is repeated across contiguous processes. 
+        qtnh::uint cycles;   ///< Number of times the entire tensor structure is repeated. 
+        qtnh::uint offset;   ///< Number of empty processes before the tensor begins. 
+        qtnh::uint base;     ///< Base distributed size of the tensor. 
+
+        const QTNHEnv& env;   ///< Environment to use MPI/OpenMP in. 
+        MPI_Comm group_comm;  ///< Communicator that contains exactly one copy of the tensor. 
+        bool active;          ///< Flag whether the tensor is stored on calling MPI rank. 
+
+        /// @brief Helper to calculate span of the entire tensor across contiguous ranks. 
+        /// @return Number of contiguous ranks that store the tensor. 
+        qtnh::uint span() const noexcept { return stretch * base * cycles; }
+        /// @brief Helper to calculate between which ranks the tensor is contained. 
+        /// @return A tuple containing first and last rank that store the tensor. 
+        std::pair<qtnh::uint, qtnh::uint> range() const noexcept { return { offset, offset + span() }; }
+      };
 
       Distributor distributor;  ///< Tensor distributor. 
-
-      qtnh::tidx_tup dims;       ///< Global index dimensions. 
-      qtnh::tidx_tup loc_dims;   ///< Local index dimensions. 
-      qtnh::tidx_tup dist_dims;  ///< Distributed index dimensions. 
   };
 }
 
