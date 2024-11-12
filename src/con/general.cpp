6830297d574be6c870dea7846940acc5f971ff3e
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2024-11-12 18:49:52 +0000

    Added implementation file for contractors

---
+++ src/con/general.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+
+#include "core/utils.hpp"
+#include "tensor/tensor.hpp"
+#include "tensor/dense.hpp"
+#include "tensor/symm.hpp"
+#include "tensor/diag.hpp"
+#include "tensor/indexing.hpp"
+#include "contract/general-temp.hpp"
+
+namespace qtnh {
+  template<> qtnh::tptr Contractor<DenseTensor, DenseTensor>::contract() {
+    return Tensor::cast<Tensor>(std::move(tp1_));
+  }
+
+  template<> qtnh::tptr Contractor<DenseTensor>::contract() {
+    return Tensor::cast<Tensor>(std::move(tp_));
+  }
+}
