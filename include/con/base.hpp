11e2a8d5c6492a15f8b683ee15d1c315349da30c
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2025-01-17 10:28:23 +0000

    Added error messages for invalid wires

--- include/con/base.hpp
+++ include/con/base.hpp
@@ -17,7 +17,24 @@ namespace qtnh {
     public:
       ContractorBase() = delete;
       ContractorBase(std::unique_ptr<T1> tp1, std::unique_ptr<T2> tp2, ConParams params)
-        : tp1_(std::move(tp1)), tp2_(std::move(tp2)), params_(params) {}
+        : tp1_(std::move(tp1)), tp2_(std::move(tp2)), params_(params) 
+        {
+          Tensor* op1 = tp1_.get();
+          Tensor* op2 = tp2_.get();;
+          if (op2 == nullptr) {
+            op2 = tp1_.get();
+          }
+
+          // Validate contraction dimensions
+          for (auto& w : params_.wires) {
+            if (op1->totDims().at(w.first) != op2->totDims().at(w.second)) {
+              throw std::invalid_argument("Incompatible contraction dimensions.");
+            }
+            if ((w.first < op1->disDims().size()) != (w.second < op2->disDims().size())) {
+              throw std::invalid_argument("Cannot contract distributed and local dimensions.");
+            }
+          }
+        }
 
       ContractorBase(const ContractorBase&) = delete;
       ContractorBase(ContractorBase&&) = default;
