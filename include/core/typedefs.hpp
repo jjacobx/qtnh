f68ed76426e185521b1ad60d1b641038a1a6da3d
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2025-01-17 12:08:53 +0000

    Changed tuple size type to size_t to fix some conversion warnings

--- include/core/typedefs.hpp
+++ include/core/typedefs.hpp
@@ -14,10 +14,11 @@ if (!_r) std::cout
 
 namespace qtnh {
   using tidx = std::size_t;          ///< Tensor index dimensions. 
-  using tidx_tup_st = unsigned int;  ///< Tensor index tuple dimensions. 
   using uint = unsigned int;         ///< Unsigned int for IDs. 
 
   using tidx_tup = std::vector<qtnh::tidx>;  ///< Tuple of tensor indices – used for accessing tensor elements. 
+  // * This might be useful in the future if tuples are reimplemented with a smaller size type. 
+  using tidx_tup_st = std::size_t;           ///< Tensor index tuple dimensions. 
 
   using tel = std::complex<double>;  ///< Tensor element type. 
 
@@ -50,7 +51,7 @@ namespace qtnh {
     out   ///< Output indices are the second parts of distributed/local dimension arrays. 
   };
 
-  constexpr qtnh::tidx_tup_st X = std::numeric_limits<tidx_tup_st>::max();
+  constexpr std::size_t X = std::numeric_limits<std::size_t>::max();
 }
 
 #endif
