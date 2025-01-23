34d85312320744bbc187bfce9e42a43a9458d809
Jakub Adamski <jakub.adamski@ed.ac.uk>
2025-01-23 15:25:46 +0000

    Improved environment functions

--- src/core/env.cpp
+++ src/core/env.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <mpi.h>
-#include <omp.h>
 
 #include "core/env.hpp"
 #include "core/typedefs.hpp"
 
 namespace qtnh {
   QTNHEnv::QTNHEnv() {
-    // num_threads = omp_get_max_threads();
     int _proc_id, _num_processes;
 
-    MPI_Init(NULL, NULL);
+    MPI_Init(0, 0);
     MPI_Comm_rank(MPI_COMM_WORLD, &_proc_id);
     MPI_Comm_size(MPI_COMM_WORLD, &_num_processes);
 
@@ -25,6 +23,6 @@ namespace qtnh {
   void QTNHEnv::print() const {
     std::cout << "Process ID: " << proc_id << std::endl;
     std::cout << "Process count: " << num_processes << std::endl;
-    // std::cout << "Thread count: " << num_threads << std::endl;
+    std::cout << "Communicator count: " << num_comms << std::endl;
   }
 }
