57a8c6a7b248cfca601fe6bf8a736814a3d169cc
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2025-02-04 18:26:17 +0000

    Added SVD routine

--- include/lalg/routines.hpp
+++ include/lalg/routines.hpp
@@ -9,5 +9,11 @@ namespace qtnh {
     // Call this to make processes independent. 
     extern "C" void blacs_get_(int*, int*, int*);
     extern "C" void blacs_gridmap_(int*, int*, int*, int*, int*, int*);
+
+    extern "C" void pzgesvd_(char* jobu, char* jobvt, int* m, int* n, 
+                             qtnh::tel* a, int* ia, int* ja, int* desc_a, 
+                             qtnh::tel* s, qtnh::tel* u, int* iu, int* ju, int* desc_u, 
+                             qtnh::tel* vt, int* ivt, int* jvt, int* desc_vt, 
+                             qtnh::tel* work, int* lwork, double* rwork, int* info);
   }
 }
