d808b15a9063822289be748bf75a7e8e13807aad
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2025-02-05 14:40:56 +0000

    Tested SVD

--- examples/test/scalapack.cpp
+++ examples/test/scalapack.cpp
@@ -22,7 +22,7 @@ int main() {
   std::cout << "1. Process: " << iam << "/" << nprocs << "; Context: " << grid.context << 
     "; Coordinates = (" << grid.row << ", " << grid.col << ")\n";
 
-  decomp::blacs_gridexit_(&grid.context);
+  if (grid.context != -1) decomp::blacs_gridexit_(&grid.context);
   utils::barrier();
 
   int nprow = 2;      // Number of row procs
@@ -74,39 +74,23 @@ int main() {
   tp = DenseTensor::make(env, {}, dims2, std::move(els));
   tp = Tensor::rescatter(std::move(tp), 3);
 
-  // Create block matrix structure
-  // (2, 2, 2) (2, 2, 2, 2) -> (2, 2; 2) (2, 2; 2, 2)
-  // 2 -> 4; 0 1 3 4 2 5 6
+  // 1. Create block matrix structure
+  // (ijk|l,mno) -> (ij,m)|(kl,no): (0 1 4 2 3 5 6)
+  // 2. Switch to column-major form
+  // (ij,m)|(kl,no) -> (ij,m)|(no,kl): (0 1 4 5 6 2 3)
 
-  std::vector<tidx_tup_st> ptup1(7, 0);
-  std::iota(ptup1.begin(), ptup1.end(), 0);
-  auto pel = ptup1.at(2);
-  ptup1.erase(ptup1.begin() + 2);
-  ptup1.insert(ptup1.begin() + 4, pel);
-
-  tp = Tensor::permute(std::move(tp), ptup1);
-  
-  // Switch to column-major form
-  // 0 1 3 4 2 5 6 -> 0 1 3 5 6 4 2
-
-  std::vector<tidx_tup_st> ptup2(7, 0);
-  std::iota(ptup2.begin(), ptup2.end(), 0);
-  auto pels = std::vector<tidx_tup_st>(ptup2.begin() + 3, ptup2.begin() + 4);
-  ptup2.erase(ptup2.begin() + 3, ptup2.begin() + 4);
-  ptup2.insert(ptup2.end(), pels.begin(), pels.end());
-
-  tp = Tensor::permute(std::move(tp), ptup2);
+  std::vector<qtnh::tidx_tup_st> ptup { 0, 1, 4, 5, 6, 2, 3 };
+  tp = Tensor::permute(std::move(tp), ptup);
   auto dtp = Tensor::convert<DenseTensor>(std::move(tp));
-  auto mels = dtp->locElsP();
 
   using namespace ops;
   std::cout << "P" << env.proc_id << ": " << *dtp << "\n";
   utils::barrier();
   
   using namespace lalg;
-  ProcGrid pgrid(4, 2);
+  ProcGrid pg42(4, 2);
 
-  BlockMatrix bm(pgrid, 16, 8, mels);
+  BlockMatrix bm(pg42, 16, 8, dtp->extractEls());
   auto desc = bm.descriptor();
 
   std::cout << "P" << env.proc_id << ": ";
@@ -118,8 +102,15 @@ int main() {
   auto mdims = bm.totDims();
   std::vector<tel> svals(8);
 
-  BlockMatrix bm_u(pgrid, 16, 8);
-  BlockMatrix bm_v(pgrid, 8, 8);
+  BlockMatrix bm_u(pg42, 16, 8);
+  BlockMatrix bm_v(pg42, 8, 8);
+
+  desc = bm_v.descriptor();
+  std::cout << "P" << env.proc_id << ": ";
+  for (auto i = 0UL; i < 9; ++i) {
+    std::cout << desc.at(i) << ", ";
+  }
+  std::cout << bm.grid().active() << "\n";
 
   auto one = 1;
 
@@ -137,12 +128,46 @@ int main() {
     auto v_desc = bm_v.descriptor();
     auto v_desc_d = const_cast<int*>(v_desc.data());
     pzgesvd_(&jobu, &jobvt, &mdims.first, &mdims.second, bm.data(), &one, &one, a_desc_d, svals.data(), 
-            bm_u.data(), &one, &one, u_desc_d, bm_v.data(), &one, &one, v_desc_d, 
-            work2, &lwork2, rwork2, &info2);
+             bm_u.data(), &one, &one, u_desc_d, bm_v.data(), &one, &one, v_desc_d, 
+             work2, &lwork2, rwork2, &info2);
   }
 
-  // Needed to prevent double-freeing. 
-  bm.extractEls().release();
+  tptr tp_u = DenseTensor::make(env, { 2, 2, 2 }, { 2, 2, 2, 2 }, std::move(bm_u.extractEls()));
+  std::vector<qtnh::tidx_tup_st> ptup_u { 0, 1, 5, 6, 2, 3, 4 };
+  tp_u = Tensor::permute(std::move(tp_u), ptup_u);
+  std::cout << "P" << env.proc_id << ": U = " << *tp_u << "\n";
+  utils::barrier();
+
+  tptr tp_v = DenseTensor::make(env, { 2, 2, 2 }, { 2, 2, 2 }, std::move(bm_v.extractEls()));
+  std::vector<qtnh::tidx_tup_st> ptup_v { 0, 2, 1, 3, 4, 5 };
+  tp_v = Tensor::permute(std::move(tp_v), ptup_v);
+  tp_v = Tensor::rescatter(std::move(tp_v), -1);
+
+  ptup_v = { 0, 3, 1, 4, 5, 2 };
+  tp_v = Tensor::permute(std::move(tp_v), ptup_v);
+  std::cout << "P" << env.proc_id << ": V = " << *tp_v << "\n";
+  utils::barrier();
+
+  tptr tp_s = DiagTensor::make(env, {}, { 2, 2, 2, 2, 2, 2 }, false, std::move(svals));
+  tp_s = Tensor::convert<DenseTensor>(std::move(tp_s));
+  tp_s = Tensor::permute(std::move(tp_s), { 0, 2, 3, 1, 4, 5 });
+  tp_s = Tensor::rescatter(std::move(tp_s), 2);
+  std::cout << "P" << env.proc_id << ": S = " << *tp_s << "\n";
+  utils::barrier();
+
+  auto params = ConParams({{ 1, 0 }, { 4, 2 }, { 5, 3 }});
+  auto con = pcon(std::move(tp_s), std::move(tp_v), params);
+  auto tp_sv = con.contract();
+
+  std::cout << "P" << env.proc_id << ": SV = " << *tp_sv << "\n";
+  utils::barrier();
+
+  params = ConParams({{ 2, 0 }, { 5, 2 }, { 6, 3 }});
+  con = pcon(std::move(tp_u), std::move(tp_sv), params);
+  auto tp_usv = con.contract();
+
+  std::cout << "P" << env.proc_id << ": USV = " << *tp_usv << "\n";
+  utils::barrier();
 
   return 0;
 }
