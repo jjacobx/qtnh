cca02981b85970e6ad0e3f87a123b66136d1c76b
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2025-02-10 15:29:22 +0000

    Added block-cyclic matrix class

--- include/lalg/matrix.hpp
+++ include/lalg/matrix.hpp
@@ -72,6 +72,52 @@ namespace qtnh {
         // Remember the elements need to be in column-major order. 
         cvec loc_els_p_;        
     };
+
+    class BlockCyclicMatrix {
+      public:
+        BlockCyclicMatrix() = delete;
+        BlockCyclicMatrix(const ProcGrid& grid, int nrows, int ncols, int nblock);
+        BlockCyclicMatrix(const ProcGrid& grid, int nrows, int ncols, int nblock, cvec&& loc_els_p);
+        ~BlockCyclicMatrix() = default;
+
+        const ProcGrid& grid() const { return grid_; }
+        qtnh::tel* data() { return loc_els_p_.data(); }
+
+        constexpr std::pair<int, int> totDims() const { return { nrows_, ncols_ }; }
+        constexpr std::pair<int, int> cycDims() const { 
+          return { 
+            nrows_ / grid_.procDims().first / nblock_, 
+            ncols_ / grid_.procDims().second / nblock_ 
+          };
+        }
+        constexpr int nBlock() const { return nblock_; }
+
+        constexpr std::array<int, 9> const descriptor() {
+          return { 
+            1,                          // DTYPE
+            grid_.context(),            // CTXT
+            nrows_,                     // M
+            ncols_,                     // N
+            nblock_,                    // MB
+            nblock_,                    // NB
+            0,                          // RSRC
+            0,                          // CSRC
+            nblock_ * cycDims().first   // LLD
+          };
+        }
+
+        cvec&& extractEls() { return std::move(loc_els_p_); }
+
+      private:
+        const ProcGrid& grid_;
+
+        int nrows_;
+        int ncols_;
+        int nblock_;
+
+        // Remember the elements need to be in column-major order. 
+        cvec loc_els_p_;   
+    };
   }
 }
 
--- src/lalg/matrix.cpp
+++ src/lalg/matrix.cpp
@@ -21,8 +21,18 @@ namespace qtnh {
       auto loc_size = (nrows * ncols) / (proc_dims.first * proc_dims.second);
       loc_els_p_ = cvec(loc_size);
     }
-    
+
     BlockMatrix::BlockMatrix(const ProcGrid& grid, int nrows, int ncols, cvec&& loc_els_p)
       : grid_(grid), nrows_(nrows), ncols_(ncols), loc_els_p_(loc_els_p) {}
+
+    BlockCyclicMatrix::BlockCyclicMatrix(const ProcGrid& grid, int nrows, int nblock, int ncols)
+      : BlockCyclicMatrix(grid, nrows, ncols, nblock, {}) {
+        auto proc_dims = grid.procDims();
+        auto loc_size = (nrows * ncols) / (proc_dims.first * proc_dims.second);
+        loc_els_p_ = cvec(loc_size);
+      }
+
+    BlockCyclicMatrix::BlockCyclicMatrix(const ProcGrid& grid, int nrows, int ncols, int nblock, cvec&& loc_els_p)
+      : grid_(grid), nrows_(nrows), ncols_(ncols), nblock_(nblock), loc_els_p_(loc_els_p) {}
   }
 }
