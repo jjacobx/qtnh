baf2faeda853b48eec6df17405da1ba3904f1009
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2025-02-17 09:46:32 +0000

    Tidied up SVD example

--- examples/test/svd.cpp
+++ examples/test/svd.cpp
@@ -12,101 +12,46 @@ int main() {
   auto els = std::vector<tel>(utils::dims_to_size(dims), 0);
   std::iota(els.begin(), els.end(), 0);
 
-  tptr tp = DenseTensor::make(env, {}, dims, std::move(els));
+  tptr tp_m = DenseTensor::make(env, {}, dims, std::move(els));
 
-  auto ndis_dims = 3UL;
-  tp = Tensor::rescatter(std::move(tp), ndis_dims);
-
-  auto dis_sep = 2UL;
-  auto loc_sep = 2UL;
-
-  PTupleSrc ptup(dims.size());
-  auto [tup_r, tup_c] = utils::split_vec(ptup.tup(), dis_sep + loc_sep);
-  auto [tup_rd, tup_rb] = utils::split_vec(tup_r, dis_sep);
-  auto [tup_cd, tup_cb] = utils::split_vec(tup_c, ndis_dims - dis_sep);
-
-  IndexGroup ig({ "rd", "cd", "cb", "rb" }, { tup_rd, tup_cd, tup_cb, tup_rb });
-  if (utils::is_root()) std::cout << ig.ptup().inv().tup() << "\n";
-
-  tp = Tensor::permute(std::move(tp), ig.ptup().toTar().tup());
-  auto dtp = Tensor::convert<DenseTensor>(std::move(tp));
-
-  auto [dims_rd, dims_cd] = utils::split_dims(dtp->disDims(), dis_sep);
-  auto [dims_cb, dims_rb] = utils::split_dims(dtp->locDims(), dtp->locDims().size() - loc_sep);
-  auto size_rd = utils::dims_to_size(dims_rd), size_cd = utils::dims_to_size(dims_cd);
-  auto size_cb = utils::dims_to_size(dims_cb), size_rb = utils::dims_to_size(dims_rb);
- 
-  using namespace lalg;
-  ProcGrid pg(size_rd, size_cd);
-
-  BlockMatrix bm(pg, size_rd * size_rb, size_cd * size_cb, dtp->extractEls());
-  auto [u, s, v] = PZGESVD(std::move(bm));
-
-  if (utils::is_root()) std::cout << "S = " << s << "\n";
-
-  tptr tp_u = DenseTensor::make(env, { 2, 2, 2 }, { 2, 2, 2, 2 }, u.extractEls());
-  tptr tp_v = DenseTensor::make(env, { 2, 2, 2 }, { 2, 2, 2 }, v.extractEls());
-  tptr tp_s = DiagTensor::make(env, {}, { 2, 2, 2, 2, 2, 2 }, false, std::move(s));
-
-  tp_u = Tensor::rescatter(std::move(tp_u), -1);
-  tp_u = Tensor::permute(std::move(tp_u), ig.ptup().toTar().inv().tup());
-  tp_v = Tensor::permute(std::move(tp_v), PTupleSrc({ 2, 0, 1, 5, 3, 4 }).toTar().tup());
-  tp_v = Tensor::rescatter(std::move(tp_v), -2);
-  tp_s = Tensor::convert<DenseTensor>(std::move(tp_s));
-
-  auto params = ConParams({{ 3, 1 }, { 4, 2 }, { 5, 3 }});
-  auto con = pcon(std::move(tp_s), std::move(tp_v), params);
-  auto tp_sv = con.contract();
-
-  params = ConParams({{ 4, 1 }, { 5, 2 }, { 6, 3 }});
-  con = pcon(std::move(tp_u), std::move(tp_sv), params);
-  auto tp_usv = con.contract();
-
-  tp_usv = Tensor::permute(std::move(tp_usv), PTupleSrc({ 0, 1, 3, 4, 2, 5, 6 }).toTar().tup());
-
-  std::cout << "P" << env.proc_id << ": USV = " << *tp_usv << "\n";
-  utils::barrier();
-
-  dims = tidx_tup { 2, 2, 2, 2, 2, 2, 2 };
-  els = std::vector<tel>(utils::dims_to_size(dims), 0);
-  std::iota(els.begin(), els.end(), 0);
-
-  tp = DenseTensor::make(env, {}, dims, std::move(els));
-  tp = Tensor::rescatter(std::move(tp), 2);
+  auto ndis_dims = 2UL;
+  tp_m = Tensor::rescatter(std::move(tp_m), ndis_dims);
 
   DecParams dp {{ 1, 1 }, { 3, 2 }, { 2, 1 }, { 1, 1 }, { 1, 1 }};
-  Decomposer dec(std::move(tp), dp);
+  Decomposer dec(std::move(tp_m), dp);
   dec.decompose();
 
-  auto [tu, ts, tv] = dec.extract_results();
+  auto [tp_u, tp_s, tp_v] = dec.extract_results();
 
-  std::cout << "P" << env.proc_id << ": U = " << *tu << "\n";
-  std::cout << "P" << env.proc_id << ": S = " << *ts << "\n";
-  std::cout << "P" << env.proc_id << ": V = " << *tv << "\n";
+  std::cout << "P" << env.proc_id << ": U = " << *tp_u << "\n";
+  std::cout << "P" << env.proc_id << ": S = " << *tp_s << "\n";
+  std::cout << "P" << env.proc_id << ": V = " << *tp_v << "\n";
   
-  ts = DiagTensor::make(env, {}, utils::concat_dims(ts->totDims(), ts->totDims()), false, ts->cast<DenseTensor>()->extractEls());
-  ts = Tensor::convert<DenseTensor>(std::move(ts));
+  auto&& els_s = tp_s->cast<DenseTensor>()->extractEls();
+  tp_s = DiagTensor::make(env, {}, utils::concat_dims(tp_s->totDims(), tp_s->totDims()), 0, std::move(els_s));
+  tp_s = Tensor::convert<DenseTensor>(std::move(tp_s));
   
-  PTupleSrc s_ptup(ts->totDims().size());
+  PTupleSrc s_ptup(tp_s->totDims().size());
   auto [tup1, tup_r1] = utils::split_dims(s_ptup.tup(), 1);
   auto [tup2, tup_r2] = utils::split_dims(tup_r1, 2);
   auto [tup3, tup4] = utils::split_dims(tup_r2, 1);
   IndexGroup igs({ "d1", "l1", "d2", "l2" }, { tup1, tup2, tup3, tup4 });
   igs.reorder({ "d1", "d2", "l1", "l2" });
 
-  ts = Tensor::permute(std::move(ts), igs.ptup().toTar().tup());
-  ts = Tensor::rescatter(std::move(ts), 2);
+  tp_s = Tensor::permute(std::move(tp_s), igs.ptup().toTar().tup());
+  tp_s = Tensor::rescatter(std::move(tp_s), 2);
 
-  params = ConParams({{ 1, 0 }, { 4, 2 }, { 5, 3 }});
-  con = pcon(std::move(ts), std::move(tv), params);
-  auto tsv = con.contract();
+  auto params = ConParams({{ 1, 0 }, { 4, 2 }, { 5, 3 }});
+  auto con = pcon(std::move(tp_s), std::move(tp_v), params);
+  auto tp_sv = con.contract();
 
   params = ConParams({{ 1, 0 }, { 5, 2 }, { 6, 3 }});
-  con = pcon(std::move(tu), std::move(tsv), params);
-  auto tusv = con.contract();
-  tusv = Tensor::rebcast(std::move(tusv), { 1, 1, 0 });
+  con = pcon(std::move(tp_u), std::move(tp_sv), params);
+  auto tp_usv = con.contract();
 
-  std::cout << "P" << env.proc_id << ": USV = " << *tusv << "\n";
+  tp_usv = Tensor::rebcast(std::move(tp_usv), { 1, 1, 0 });
+
+  std::cout << "P" << env.proc_id << ": USV = " << *tp_usv << "\n";
 
   return 0;
 }
