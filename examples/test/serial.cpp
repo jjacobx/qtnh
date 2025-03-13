f41fef2935e5e93904f1f298b305934e9278b8d9
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2025-03-13 12:00:41 +0000

    Found failing example on ARCHER2

--- examples/test/serial.cpp
+++ examples/test/serial.cpp
@@ -10,10 +10,10 @@ using namespace std::complex_literals;
 int main() {
   QTNHEnv env;
 
-  tptr tp1 = DenseTensor::make(env, {}, { 2, 2 }, { 1, 2, 3, 4 });
-  tptr tp2 = DenseTensor::make(env, {}, { 2, 2 }, { 1, 0, 0, 1 });
+  tptr tp1 = DenseTensor::make(env, {}, { 2, 2 }, { 1, 0, 0, 1 });
+  tptr tp2 = DenseTensor::make(env, {}, { 2, 2, 2, 2 }, { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 });
 
-  pcon con(std::move(tp1), std::move(tp2), ConParams(std::vector<wire> {{ 1, 0 }}));
+  pcon con(std::move(tp1), std::move(tp2), ConParams(std::vector<wire> {{ 0, 2 }}));
   tptr tp3 = con.contract();
 
   std::cout << *tp3 << "\n";
