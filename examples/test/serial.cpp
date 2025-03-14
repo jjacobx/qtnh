0e4f7241e36fecfb2f0dfc7fd744fbd9e003aa60
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2025-03-14 09:43:07 +0000

    Added test for permutation bug

--- examples/test/serial.cpp
+++ examples/test/serial.cpp
@@ -13,8 +13,10 @@ int main() {
   tptr tp1 = DenseTensor::make(env, {}, { 2, 2 }, { 1, 0, 0, 1 });
   tptr tp2 = DenseTensor::make(env, {}, { 2, 2, 2, 2 }, { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 });
 
-  pcon con(std::move(tp1), std::move(tp2), ConParams(std::vector<wire> {{ 0, 2 }}));
+  pcon con(tp1->copy(), tp2->copy(), ConParams(std::vector<wire> {{ 0, 2 }}));
   tptr tp3 = con.contract();
-
   std::cout << *tp3 << "\n";
+
+  tp2 = Tensor::permute(std::move(tp2), { 0, 1, 3, 2 });
+  std::cout << *tp2 << "\n";
 }
