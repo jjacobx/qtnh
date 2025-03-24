0e6fcb728598b177403c67c21fc1d54136cad5c1
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2025-03-24 15:08:50 +0000

    Tested MPS

--- examples/test/mps.cpp
+++ examples/test/mps.cpp
@@ -8,31 +8,42 @@ using namespace std::complex_literals;
 int main() {
   QTNHEnv env;
 
-  constexpr auto N_SITES = 4UL;
+  constexpr auto N_SITES  = 5UL;
   constexpr auto SITE_DIM = 2UL;
-  constexpr auto CHI_DIS = 2UL;
-  constexpr auto CHI_LOC = 2UL;
+  constexpr auto CHI_DIS  = 2UL;
+  constexpr auto CHI_LOC  = 3UL;
 
   MPS mps(env, N_SITES, SITE_DIM, { CHI_DIS, CHI_LOC });
 
   mps.print();
 
-  std::vector<tel> els = {
-    1,  1,  1,  1, 
-    1, -1,  1, -1, 
-    1,  1, -1, -1, 
-    1, -1, -1,  1
+  std::vector<tel> els_h = {
+    1 / std::sqrt(2),  1 / std::sqrt(2), 
+    1 / std::sqrt(2), -1 / std::sqrt(2)
   };
 
-  std::transform(els.begin(), els.end(), els.begin(), [](auto e) { return e / std::sqrt(2); });
+  std::vector<tel> els_hh = {
+    .5,  .5,  .5,  .5, 
+    .5, -.5,  .5, -.5, 
+    .5,  .5, -.5, -.5, 
+    .5, -.5, -.5,  .5
+  };
 
-  auto op = SymmTensor::make(env, {}, tidx_tup(4, SITE_DIM), std::move(els));
+  std::vector<tel> els_cx = {
+    1, 0, 0, 0, 
+    0, 1, 0, 0, 
+    0, 0, 0, 1, 
+    0, 0, 1, 0
+  };
+
+  auto h =  SymmTensor::make(env, {}, tidx_tup(2, SITE_DIM), std::move(els_h));
+  auto hh = SymmTensor::make(env, {}, tidx_tup(4, SITE_DIM), std::move(els_hh));
+  auto cx = SymmTensor::make(env, {}, tidx_tup(4, SITE_DIM), std::move(els_cx));
 
   utils::barrier();
 
-  if (utils::is_root()) std::cout << "APPLYING OPERATOR\n";
-  mps.apply(Tensor::cast<SymmTensor>(op->copy()), { 1, 2 });
-  mps.apply(Tensor::cast<SymmTensor>(op->copy()), { 1, 2 });
+  mps.apply(Tensor::cast<SymmTensor>(h->copy()), { 0 });
+  mps.apply(Tensor::cast<SymmTensor>(cx->copy()), { 0, 2 });
 
   mps.print();
 
