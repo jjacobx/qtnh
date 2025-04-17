f0a58175bb7fae931fe5a50aa1d3dbffec9dcb92
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2025-04-17 10:45:54 +0100

    Added map flipping function

--- include/util/utils.hpp
+++ include/util/utils.hpp
@@ -1,6 +1,8 @@
 #ifndef __UTIL_UTILS__
 #define __UTIL_UTILS__
 
+#include <algorithm>
+#include <map>
 #include <memory>
 #include <string>
 
@@ -102,6 +104,21 @@ namespace qtnh {
 
     namespace binops {
       void add_sq(void* a, void* b, int*, MPI_Datatype*);
+    }
+
+    // Source: https://stackoverflow.com/questions/5056645/sorting-stdmap-using-value. 
+    template<typename A, typename B>
+    std::pair<B,A> flip_pair(const std::pair<A,B> &p)
+    {
+        return std::pair<B,A>(p.second, p.first);
+    }
+
+    template<typename A, typename B>
+    std::multimap<B,A> flip_map(const std::map<A,B> &src)
+    {
+      std::multimap<B,A> dst;
+      std::transform(src.begin(), src.end(), std::inserter(dst, dst.begin()), flip_pair<A,B>);
+      return dst;
     }
   }
 }
