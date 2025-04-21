cb23b3f56fa15a8de6f8ab20f9782700230654f7
Jakub Adamski <Jakub.Adamski@ed.ac.uk>
2025-04-21 11:39:51 +0100

    Implemented custom contraction QFT network

--- examples/ieee25/qftn.cpp
+++ examples/ieee25/qftn.cpp
@@ -1,53 +1,20 @@
 #include <chrono>
 #include <iostream>
-#include <map>
 #include "qtnh.hpp"
 
 using namespace qtnh;
 using namespace std::chrono;
 using namespace std::complex_literals;
 
-auto qft_triangle(const QTNHEnv& env, TensorNetwork& tn, MPS& mps) {
-  auto n = mps.nSites();
-  std::map<std::pair<std::size_t, std::size_t>, qtnh::uint> grid;
+using grid_t = std::map<std::pair<std::size_t, std::size_t>, qtnh::uint>;
 
-  // Prepare initial state. 
-  mps.apply(qops::h(env), { 0 });
-  mps.rightCanonicalise(0);
+grid_t qft_triangle(const QTNHEnv& env, TensorNetwork& tn, uint tid_state) {
+  auto n = tn.tensor(tid_state)->totDims().size();
+  auto d = tn.tensor(tid_state)->disDims().size();
+  grid_t grid;
 
-  MPO mpo = qops::cmp(env, n);
-  mpo.rightCanonicalise();
-  mps.apply(mpo, 0);
-
-  // Insert initial state. 
-  for (auto i = 0UL; i < n; ++i) {
-    tptr tp = mps.site(i).copy();
-    auto k = mps.totChi();
-
-    tp = Tensor::rescatter(std::move(tp), -2);
-    tp = Tensor::permute(std::move(tp), { 0, 3, 2, 1, 4 });
-    tp->reshape({}, { mps.totChi(), mps.siteDims().at(i), mps.totChi(), 1 });
-
-    // Special cases when first/last site. 
-    if (i == 0UL) { 
-      tp = Tensor::truncate(std::move(tp), 0, 1);
-    } else if (i == n - 1) {
-      tp = Tensor::truncate(std::move(tp), 2, 1);
-    }
-
-    // Another special case – Hadamard gate. 
-    if (i == 1UL) {
-      pcon con(std::move(tp), qops::h(env), ConParams({{ 1, 0 }}));
-      tp = con.contract();
-    }
-
-    auto tid = tn.insert(std::move(tp));
-    grid.insert({ { n - i - 1, 0 }, tid });
-  }
-
-  for (auto i = 1UL; i + 1 < n; ++i) {
+  for (auto i = 0UL; i + 1 < n; ++i) {
     MPO mpo = qops::cmp(env, n - i);
-    mpo.rightCanonicalise();
 
     // Insert operators. 
     auto m = mpo.nSites();
@@ -63,13 +30,17 @@ auto qft_triangle(const QTNHEnv& env, TensorNetwork& tn, MPS& mps) {
         tp->reshape({}, loc_shape);
       }
 
-      // TODO: Check convention. 
-      tp = Tensor::permute(std::move(tp), { 3, 1, 0, 2 });
-
-      // Add Hadamard gate for second operator. 
-      if (j == 1UL) {
+      // Add Hadamard gates. 
+      if (i == 0UL && j == 0UL) {
+        pcon con(std::move(tp), qops::h(env), ConParams({{ 0, 0 }}));
+        tp = con.contract();
+      } else if (j == 1UL) {
         pcon con(std::move(tp), qops::h(env), ConParams({{ 1, 0 }}));
         tp = con.contract();
+      }
+
+      if (i + j < d) {
+        tp = Tensor::rescatter(std::move(tp), 2);
       }
 
       auto tid = tn.insert(std::move(tp));
@@ -79,53 +50,66 @@ auto qft_triangle(const QTNHEnv& env, TensorNetwork& tn, MPS& mps) {
 
   // Connect tensors with bonds. 
   for (auto& [p, i] : grid) {
+    if (p.second == 0) {
+      tn.addBond(tid_state, i, {{ n - p.first - 1, 0 }});
+    }
     if (grid.find({ p.first + 1, p.second }) != grid.end()) {
-      tn.addBond(i, grid.at({ p.first + 1, p.second }), {{ 0, 2 }});
+      tn.addBond(i, grid.at({ p.first + 1, p.second }), {{ 2, 3 }});
     }
     if (grid.find({ p.first, p.second + 1 }) != grid.end()) {
-      tn.addBond(i, grid.at({ p.first, p.second + 1 }), {{ 1, 3 }});
+      tn.addBond(i, grid.at({ p.first, p.second + 1 }), {{ 1, 0 }});
     }
   }
 
   return grid;
 }
 
+uint contract1(TensorNetwork& tn, grid_t& grid, uint tid_state) {
+  auto n = tn.tensor(tid_state)->totDims().size();
+  for (auto i = 0UL; i + 1 < n; ++i) {
+    for (auto j = 0UL; j < n - i; ++j) {
+      tid_state = tn.contractTensors(tid_state, grid.at({ n - i - j - 1, i }));
+    }
+  }
+
+  return tid_state;
+}
+
 int main(int argc, char* argv[]) {
   constexpr auto SITE_DIM = 2UL;
   
-  auto N_SITES = 4UL;
-  auto CHI_DIS = 2UL;
-  auto CHI_LOC = 2UL;
+  auto DQ = 2UL;
+  auto LQ = 2UL;
 
-  if (argc > 1) {
-    N_SITES = static_cast<unsigned int>(strtol(argv[1], nullptr, 0));
-  }
-  if (argc > 3) {
-    CHI_DIS = static_cast<unsigned int>(strtol(argv[2], nullptr, 0));
-    CHI_LOC = static_cast<unsigned int>(strtol(argv[3], nullptr, 0));
+  if (argc > 2) {
+    DQ = static_cast<unsigned int>(strtol(argv[1], nullptr, 0));
+    LQ = static_cast<unsigned int>(strtol(argv[2], nullptr, 0));
   }
 
   QTNHEnv env;
-  MPS mps(env, N_SITES, SITE_DIM, { CHI_DIS, CHI_LOC });
+
+  std::vector<tel> tels(1 << LQ, 0);
+  if (utils::is_root()) tels.at(0) = 1;
+  tptr tp_state = DenseTensor::make(env, tidx_tup(DQ, 2), tidx_tup(LQ, 2), std::move(tels));
 
   TensorNetwork tn;
-  auto grid = qft_triangle(env, tn, mps);
-  if (utils::is_root()) tn.print();
+  auto tid_state = tn.insert(std::move(tp_state));
+  auto grid = qft_triangle(env, tn, tid_state);
 
-  if (N_SITES == 4UL) {
-    auto tid11 = tn.contractTensors(4, 7);
-    auto tid12 = tn.contractTensors(3, 6);
-    auto tid1 = tn.contractTensors(tid11, tid12);
 
-    auto tid21 = tn.contractTensors(2, 5);
-    auto tid2 = tn.contractTensors(tid21, 1);
+  utils::barrier();
+  auto start = high_resolution_clock::now();
 
-    auto tid3 = tn.contractTensors(8, 9);
+  tid_state = contract1(tn, grid, tid_state);
 
-    auto tid4 = tn.contractTensors(tid1, tid2);
-    auto tid_final = tn.contractTensors(tid3, tid4);
+  utils::barrier();
+  auto stop = high_resolution_clock::now();
+  auto delta = duration_cast<milliseconds>(stop - start);
 
-    tptr tp = tn.extract(tid_final);
-    tp->print_serial("Final");
+  tptr tp = tn.extract(tid_state);
+  tp->reshape(tidx_tup(DQ, 2), tidx_tup(LQ, 2));
+  if (utils::is_root()) {
+    std::cout << "Result: " << (*tp)[0] << "\n";
+    std::cout << "Time taken: " << delta.count() << " ms\n";
   }
 }
