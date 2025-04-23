a45eae576527f34b030152ebb36e6d58a47abe04
Jakub Adamski <jakub.adamski@ed.ac.uk>
2025-04-23 16:09:36 +0100

    Noted down scrape parameters

---
+++ run/res/scrape-ieee25.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+module load cray-R
+
+Rscript scrape.R ieee25/qftsv "^Contracted .* bonds$" "^Starting contraction" "^$"
+Rscript scrape.R ieee25/qftn "^Contracted .* tensors$" "^$"
+Rscript scrape.R ieee25/qftmps "^Iteration [0-9]+/[0-9]+$" "^$"
+Rscript scrape.R ieee25/qftrmps "^Iteration [0-9]+/[0-9]+$" "^$"
