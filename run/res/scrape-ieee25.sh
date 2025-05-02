9fb5b452bbaa61d7c9009f6e160b35d76b1d633c
Jakub Adamski <jakub.adamski@ed.ac.uk>
2025-05-02 10:06:09 +0100

    Scraped RCS MPS results

--- run/res/scrape-ieee25.sh
+++ run/res/scrape-ieee25.sh
@@ -6,3 +6,4 @@ Rscript scrape.R ieee25/qftsv "^Contracted .* bonds$" "^Starting contraction" "^
 Rscript scrape.R ieee25/qftn "^Contracted .* tensors$" "^$"
 Rscript scrape.R ieee25/qftmps "^Iteration [0-9]+/[0-9]+$" "^$"
 Rscript scrape.R ieee25/qftrmps "^Iteration [0-9]+/[0-9]+$" "^$"
+Rscript scrape.R ieee25/rcsmps "^Iteration [0-9]+/[0-9]+$" "^(\\([0-9]+, [0-9]+\\)(, )?)+$" "^bonds = .*$" "^norm = .*$" "^$"
