4e6e13523050ce46f8e43d79c5e39debdbf8b236
Jakub Adamski <jakub.adamski@ed.ac.uk>
2025-10-15 16:47:03 +0100

    Moved permute benchmarking script

---
+++ run/exp/bench/permute.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+run_permute() {
+  local nq=$1   # total qubits
+  local dq=$2   # distributed qubits
+  local mpi=$3  # MPI implementation
+  local cpn=128 # cores per node
+
+  local n_proc=$((1 << $dq))
+  local n_node=$((($n_proc - 1) / $cpn + 1))
+  echo "n_proc=$n_proc,n_node=$n_node"
+
+  local out=out/bench/%j.out
+  local exp=bench-permute
+  local prog=examples/bench/permute
+  local args="$nq $dq 100"
+
+  sbatch -D $QTNH_DIR/run -N $n_node -n $n_proc -o $out \
+    --export=EXP=$exp,PROG=$prog,ARGS="$args",MPI_IMPL=$mpi \
+    $QTNH_DIR/run/run.slurm
+}
+
+DQS=$(seq 0 2)
+
+for dq in ${DQS}; do
+  wait_running_jobs 40
+  run_permute $(($dq + 20)) $dq OFI
+done
+
+# for dq in ${DQS}; do
+#   wait_running_jobs 40
+#   run_permute $(($dq + 22)) $dq OFI
+# done
+
+# for dq in ${DQS}; do
+#   wait_running_jobs 40
+#   run_permute $(($dq + 24)) $dq OFI
+# done
+
+# for dq in ${DQS}; do
+#   wait_running_jobs 40
+#   run_permute $(($dq + 20)) $dq UCX
+# done
+
+# for dq in ${DQS}; do
+#   wait_running_jobs 40
+#   run_permute $(($dq + 22)) $dq UCX
+# done
+
+# for dq in ${DQS}; do
+#   wait_running_jobs 40
+#   run_permute $(($dq + 24)) $dq UCX
+# done
