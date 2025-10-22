17198a1fe3069f8354b64d67d157626827829f7f
Jakub Adamski <jakub.adamski@ed.ac.uk>
2025-10-22 13:00:54 +0100

    Added moves to constructors

--- src/blas/matrix.cpp
+++ src/blas/matrix.cpp
@@ -74,7 +74,7 @@ namespace qtnh {
     , nc_(tot_dims.second / nd_ / nb_)
     , m_(tot_dims.first)
     , n_(tot_dims.second)
-    , loc_els_(loc_els)
+    , loc_els_(std::move(loc_els))
     {}
 
     BlockCyclicMatrix::BlockCyclicMatrix(const ProcGrid& pg, mtup blk_dims, mtup dis_dims, mtup cyc_dims)
@@ -95,7 +95,7 @@ namespace qtnh {
     , nc_(cyc_dims.second)
     , m_(mc_ * md_ * mb_)
     , n_(nc_ * nd_ * nb_)
-    , loc_els_(loc_els)
+    , loc_els_(std::move(loc_els))
     {}
 
     BlockCyclicMatrix BlockCyclicMatrix::toGrid(const ProcGrid &pg) && {
