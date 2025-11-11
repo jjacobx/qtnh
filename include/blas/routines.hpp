5fcfb7ff47fbac324a11c650472da289c20067a5
Jakub Adamski <jakub.adamski@ed.ac.uk>
2025-11-11 12:16:15 +0000

    Moved extern keyword and added printing routine

--- include/blas/routines.hpp
+++ include/blas/routines.hpp
@@ -7,61 +7,70 @@ namespace qtnh {
   namespace lalg {
     using complex = std::complex<double>;
 
-    // Cblacs. 
-    extern "C" void Cblacs_pinfo(int* mypnum, int* nprocs);
-    extern "C" void Cblacs_get(int context, int request, int* value);
-    extern "C" void Cblacs_exit(int error_code);
-    extern "C" void Cblacs_barrier(int context, char* scope);
+    extern "C" {
+      void Cblacs_pinfo(int* mypnum, int* nprocs);
+      void Cblacs_get(int context, int request, int* value);
+      void Cblacs_exit(int error_code);
+      void Cblacs_barrier(int context, char* scope);
 
-    extern "C" void Cblacs_gridinit(int* context, char* order, int np_row, int np_col);
-    extern "C" void Cblacs_gridmap(int* context, int* usermap, int ldup, int np_row, int np_col);
-    extern "C" void Cblacs_gridinfo(int context, int* np_row, int* np_col, int* my_row, int* my_col);
-    extern "C" void Cblacs_gridexit(int context);
+      void Cblacs_gridinit(int* context, char* order, int np_row, int np_col);
+      void Cblacs_gridmap(int* context, int* usermap, int ldup, int np_row, int np_col);
+      void Cblacs_gridinfo(int context, int* np_row, int* np_col, int* my_row, int* my_col);
+      void Cblacs_gridexit(int context);
 
-    extern "C" int  Cblacs_pnum(int context, int prow, int pcol);
-    extern "C" void Cblacs_pcoord(int context, int pnum, int* prow, int* pcol);
+      int  Cblacs_pnum(int context, int prow, int pcol);
+      void Cblacs_pcoord(int context, int pnum, int* prow, int* pcol);
 
-    // ScaLAPACK. 
-    extern "C" void pzlacpy_(char* uplo, int* m, int* n, 
-                             complex* a, int* ia, int* ja, int* desc_a, 
-                             complex* b, int* ib, int* jb, int* desc_b);
-    extern "C" void pzlaset_(char* uplo, int* m, int* n, complex* alpha, complex* beta, 
-                             complex* a, int* ia, int* ja, int* desc_a);
+      // ScaLAPACK. 
+      void pzlacpy_(char* uplo, int* m, int* n, 
+                    complex* a, int* ia, int* ja, int* desc_a, 
+                    complex* b, int* ib, int* jb, int* desc_b);
+      void pzlaset_(char* uplo, int* m, int* n, complex* alpha, complex* beta, 
+                    complex* a, int* ia, int* ja, int* desc_a);
+    
+      void pzgemm_(char* transa, char* transb, 
+                   int* m, int* n, int* k, complex* alpha, 
+                   complex* a, int* ia, int* ja, int* desc_a, 
+                   complex* b, int* ib, int* jb, int* desc_b, complex* beta, 
+                   complex* c, int* ic, int* jc, int* desc_c);
+    
+      void pzgeqrf_(int* m, int* n, 
+                    complex* a, int* ia, int* ja, int* desc_a, 
+                    complex* tau, complex* work, int* lwork, int* info);
+      void pzungqr_(int* m, int* n, int* k, 
+                    complex* a, int* ia, int* ja, int* desc_a, 
+                    complex* tau, complex* work, int* lwork, int* info);
+    
+      void pzgelqf_(int* m, int* n, 
+                    complex* a, int* ia, int* ja, int* desc_a, 
+                    complex* tau, complex* work, int* lwork, int* info);
+      void pzunglq_(int* m, int* n, int* k, 
+                    complex* a, int* ia, int* ja, int* desc_a, 
+                    complex* tau, complex* work, int* lwork, int* info);
 
-    extern "C" void pzgesvd_(char* jobu, char* jobvt, int* m, int* n, 
-                             complex* a, int* ia, int* ja, int* desc_a, double* s, 
-                             complex* u, int* iu, int* ju, int* desc_u, 
-                             complex* vt, int* ivt, int* jvt, int* desc_vt, 
-                             complex* work, int* lwork, double* rwork, int* info);
-    
-    extern "C" void pzgemm_(char* transa, char* transb, int* m, int* n, int* k, complex* alpha, 
-                            complex* a, int* ia, int* ja, int* desc_a, 
-                            complex* b, int* ib, int* jb, int* desc_b, complex* beta, 
-                            complex* c, int* ic, int* jc, int* desc_c);
-    
-    extern "C" void pzgeqrf_(int* m, int* n, 
-                             complex* a, int* ia, int* ja, int* desc_a, 
-                             complex* tau, complex* work, int* lwork, int* info);
-    extern "C" void pzungqr_(int* m, int* n, int* k, 
-                             complex* a, int* ia, int* ja, int* desc_a, 
-                             complex* tau, complex* work, int* lwork, int* info);
-    
-    extern "C" void pzgelqf_(int* m, int* n, 
-                             complex* a, int* ia, int* ja, int* desc_a, 
-                             complex* tau, complex* work, int* lwork, int* info);
-    extern "C" void pzunglq_(int* m, int* n, int* k, 
-                             complex* a, int* ia, int* ja, int* desc_a, 
-                             complex* tau, complex* work, int* lwork, int* info);
+      void pzgeqpf_(int* m, int* n, 
+                    complex* a, int* ia, int* ja, int* desc_a, 
+                    int* ipiv, complex* tau, complex* work, int* lwork, 
+                    double* rwork, int* lrwork, int* info);
 
-    extern "C" void pzgeqpf_(int* m, int* n, 
-                             complex* a, int* ia, int* ja, int* desc_a, 
-                             int* ipiv, complex* tau, complex* work, int* lwork, 
-                             double* rwork, int* lrwork, int* info);
+      void pzgesvd_(char* jobu, char* jobvt, int* m, int* n, 
+                    complex* a, int* ia, int* ja, int* desc_a, double* s, 
+                    complex* u, int* iu, int* ju, int* desc_u, 
+                    complex* vt, int* ivt, int* jvt, int* desc_vt, 
+                    complex* work, int* lwork, double* rwork, int* info);
     
-    extern "C" void pzlapiv_(char* direc, char* rowcol, char* pivroc, int* m, int* n, 
-                             complex* a, int* ia, int* ja, int* desc_a, 
-                             int* ipiv, int* ip, int* jp, int* desc_ip, int* iwork);
-    
+      void pzlapiv_(char* direc, char* rowcol, char* pivroc, int* m, int* n, 
+                    complex* a, int* ia, int* ja, int* desc_a, 
+                    int* ipiv, int* ip, int* jp, int* desc_ip, int* iwork);
+      void pzlapv2_(char* direc, char* rowcol, int* m, int* n, 
+                    complex* a, int* ia, int* ja, int* desc_a, 
+                    int* ipiv, int* ip, int* jp, int* desc_ip);
+
+      void pzlaprnt_(int* m, int* n, 
+                     complex* a, int* ia, int* ja, int* desc_a, 
+                     int* irprnt, int* icprnt, 
+                     char* cmatnm, int* nout, complex* work, int size);
+    }
   }
 }
 
