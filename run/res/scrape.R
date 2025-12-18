766ceaa11e34f598bc927e2810d6e00f5653653d
Jakub Adamski <jakub.adamski@ed.ac.uk>
2025-12-18 09:53:30 +0000

    Handled aborted job outputs

--- run/res/scrape.R
+++ run/res/scrape.R
@@ -23,7 +23,7 @@ files_out <- list.files(data_dir, "*.out")
 data_table <- tibble()
 
 rm_patterns = character(0)
-if (length(length(cmd_args)) > 1) {
+if (length(cmd_args) > 1) {
   rm_patterns <- cmd_args[2:length(cmd_args)]
 }
 
@@ -35,6 +35,10 @@ for (file in files_out) {
   line_exe <- str_which(lines, "^Running.*:")
   line_start <- line_exe + 1
   line_end <- str_which(lines, "^Finished running") - 1
+
+  if (length(line_end) == 0) {
+    line_end <- length(lines)
+  }
 
   # Match metadata in the form FIELD=value
   meta_lines <- str_extract(lines[1:(line_exe - 1)], "^[A-Z_0-9]+=.*")
